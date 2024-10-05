a05ab0fc502f92a8fa0470807f55d427f14c9b08
=tod1az <=>
2024-10-05 20:32:39 -0300

    feat: add TEstServerRun and TestClientConnection

--- test/server_test.go
+++ test/server_test.go
@@ -1,11 +1,73 @@
 package test
 
 import (
+	"fmt"
+	"os"
+	"tcp/client"
 	"tcp/server"
 	"testing"
+	"time"
 )
 
-func ClientConnect(t *Testing.T) {
-	server.StartServer()
+var port = "6969"
+var host = "localhost"
+var userName1 = "Tomas"
+var userName2 = "Gonzalo"
 
+func TestServerRun(t *testing.T) {
+
+	s := server.CreateServer(host, port)
+	listener, err := s.CreateListener()
+	if err != nil {
+		t.Error("Error while creating the listener")
+	}
+
+	originalStdout := os.Stdout
+	r, w := PipeStdout()
+
+	go s.ListenForMessages(listener)
+
+	time.Sleep(100 * time.Millisecond)
+
+	outPut := GetOutput(r, w)
+	os.Stdout = originalStdout
+
+	s.StopServer()
+
+	expectedOutput := fmt.Sprintf("Listening on port: %v\n", s.Port)
+	if outPut != expectedOutput {
+		t.Errorf("expected: %v\nbut got: %v", expectedOutput, outPut)
+	}
+}
+
+func TestUserConnection(t *testing.T) {
+	thisPort := "3001"
+	s := server.CreateServer(host, thisPort)
+	listener, err := s.CreateListener()
+	if err != nil {
+		t.Error("Error while creating the listener")
+	}
+
+	go s.ListenForMessages(listener)
+
+	time.Sleep(1000 * time.Millisecond)
+	c, err := client.CreateClient(userName1, host, thisPort)
+	if err != nil {
+		t.Error("Error while creating a new client")
+	}
+
+	originalStdout := os.Stdout
+	r, w := PipeStdout()
+
+	go c.SendName()
+	time.Sleep(1000 * time.Millisecond)
+
+	outPut := GetOutput(r, w)
+	os.Stdout = originalStdout
+	s.StopServer()
+
+	expected := fmt.Sprintf("Addres: %v Name: %v connected\n", c.Conn.LocalAddr(), c.Name)
+	if outPut != expected {
+		t.Errorf("expected: %v\nbut got: %v", expected, outPut)
+	}
 }
